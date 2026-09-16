99cb261749c55f7a6d61e50800d91b108b6a9c48
Ayaz Gouri <ayazgouri.tech@gmail.com>
2026-09-16 18:52:40 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // Add elements
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+    }
+}
